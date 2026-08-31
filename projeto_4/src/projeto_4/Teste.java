cd1d2c0af72dc327c6b9caa9e5418468d3195025
biacrux <cruzb8372@gmail.com>
2026-08-31 16:09:38 +0100

    prj4  coriigido

--- projeto_4/src/projeto_4/Teste.java
+++ projeto_4/src/projeto_4/Teste.java
@@ -1,17 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projeto_4;
-import java.io.FileReader; 
+import java.io.FileReader;
 import java.io.FileNotFoundException;
 
 public class Teste {
-    public void LerArquivo(){
+    
+    public void lerArquivo(){
         try{
-            FileReader f = new  Reader("aquivo.txt")
-        }catch(FileNotException e){
-            System.out.println("o arquivo nao foi encontrado");
+            FileReader f = new FileReader("arquivo.txt");
+        }catch(FileNotFoundException e){
+            System.out.println("O arquivo não foi encontrado");
         }
     }
     
@@ -20,13 +17,14 @@ public class Teste {
     }
    
     public static void main(String[] args) {
-        metodoA();
+        Teste t = new Teste();
+        
+        t.lerArquivo(); // já trata a exceção internamente, pode chamar-se direto
+
         try{
-            metodoA()
-        }catch(FileNotFoundException e ){
+            t.metodoA();
+        }catch(FileNotFoundException e){
             System.out.println("Arquivo ausente");
         }
-                
     }
-    
 }
