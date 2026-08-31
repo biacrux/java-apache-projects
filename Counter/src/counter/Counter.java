5f19f9070950bca3aadca0bae9c8924c66e17679
biacrux <cruzb8372@gmail.com>
2026-08-31 15:32:58 +0100

    implementacao que faltava

--- Counter/src/counter/Counter.java
+++ Counter/src/counter/Counter.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package counter;
 
-/**
- *
- * @author cruzb
- */
+
 public class Counter {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int limite = 10;
+        
+        System.out.println("A contar de 1 até " + limite + ":");
+        
+        for (int i = 1; i <= limite; i++) {
+            System.out.println(i);
+        }
+        
+        System.out.println("Contagem terminada!");
     }
-    
 }
